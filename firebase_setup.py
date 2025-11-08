d3ba8956d7e37d98eb833d5fcdca88488e1a80cf
uttampd07 <uttampd2103@gmail.com>
2025-11-08 18:10:46 +0530

    Change environment variable for Firebase key

--- firebase_setup.py
+++ firebase_setup.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os, json
 
-if "FIREBASE_KEY" in os.environ:
+if "979070ab24c9f4b7d9785d254359d810d8f11b88" in os.environ:
     # Read key from environment (Render)
-    key_dict = json.loads(os.environ["FIREBASE_KEY"])
+    key_dict = json.loads(os.environ["979070ab24c9f4b7d9785d254359d810d8f11b88"])
     cred = credentials.Certificate(key_dict)
 else:
     # Local fallback for your PC
